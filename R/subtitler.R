21ebcbd527486c63a07704805745a271359944eb
Ben Cunningham <benjamescunningham@gmail.com>
2017-02-21 22:24:37 -0500

    Add man page, other setup for package

---
+++ R/subtitler.R
@@ -0,0 +1,13 @@
+#' subtitler
+#'
+#' Read and write video subtitle formats.
+#'
+#' See the README on \href{https://github.com/benjcunningham/subtitler}{GitHub}.
+#'
+#' @docType package
+#' @name subtitler
+#' @importFrom dplyr %>%
+NULL
+
+# Quiets R CMD check NOTEs for `.` in pipelines
+utils::globalVariables(c("."))
