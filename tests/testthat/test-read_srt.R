af512323d21974f7f176482710367d05232c105a
Ben Cunningham <benjamescunningham@gmail.com>
2017-02-27 21:51:57 -0500

    Add timestamp tests

--- tests/testthat/test-read_srt.R
+++ tests/testthat/test-read_srt.R
@@ -1,12 +1,41 @@
-context("Read SubRip Files")
+context("Work with SubRip Files")
 
-library(readr)
+suppressMessages({
+
+  library(dplyr)
+  library(readr)
+
+  srt <-
+    read_srt("charlie_work.srt") %>%
+    head(3) %>%
+    mutate(
+      start_ms = as_milliseconds(start),
+      end_ms = as_milliseconds(end)
+    )
+
+  csv <- read_csv("charlie_work.csv")
+
+})
+
 
 test_that("Read in data frame looks as expected", {
 
-  srt <- read_srt("charlie_work.srt")
-  csv <- read_csv("charlie_work.csv")
+  cols <- c("index", "start", "end", "text")
 
-  expect_equal(srt[1:3, ], csv[1:3, ])
+  expect_equal(srt[cols], csv[cols])
+
+})
+
+test_that("Timestamps convert to milliseconds", {
+
+  expect_equal(srt$start_ms, csv$start_ms)
+  expect_equal(srt$end_ms, csv$end_ms)
+
+})
+
+test_that("Milliseconds convert to timestamps", {
+
+  expect_equal(as_timestamp(srt$start_ms), srt$start)
+  expect_equal(as_timestamp(srt$end_ms), srt$end)
 
 })
