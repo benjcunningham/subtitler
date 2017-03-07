0bba9557e1340ba88bf4ba1ba87db8aef19f552e
Ben Cunningham <benjamescunningham@gmail.com>
2017-03-06 22:44:09 -0500

    Add add_milliseconds convenience function
    Closes #2.

---
+++ R/add_milliseconds.R
@@ -0,0 +1,18 @@
+#' Add Milliseconds to a SubRip (SRT) Timestamp
+#'
+#' Convenience method for directly adding milliseconds to a timestamp.
+#' Equivalent to \code{as_timestamp(as_milliseconds(x) + ms)}.
+#'
+#' @param x Vector of timestamps.
+#' @param ms Numeric vector of milliseconds to add.
+#'
+#' @return A character vector corresponding to x.
+#'
+#' @export
+add_milliseconds <- function(x, ms) {
+
+  as_milliseconds(x) %>%
+    `+`(ms) %>%
+    as_timestamp()
+
+}
