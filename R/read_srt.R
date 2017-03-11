0a3ebd05b3d0aa1ea08cf843b608d76c2a03950b
Ben Cunningham <benjamescunningham@gmail.com>
2017-03-10 22:31:04 -0500

    Speed up read_srt

--- R/read_srt.R
+++ R/read_srt.R
@@ -9,31 +9,13 @@
 #' @export
 read_srt <- function(file) {
 
-  subs <-
-    readr::read_file(file) %>%
-    stringr::str_split("(\r\n){2,}|\n{2,}") %>%
-    unlist()
-
-  purrr::map_df(subs[subs != ""], function(x) {
-
-    sub <- unlist(stringr::str_split(x, "(\r\n)|\n"))
-
-    tibble::data_frame(
-      index =
-        sub[1] %>%
-        as.integer(),
-      start =
-        sub[2] %>%
-        stringr::str_extract("^\\d+:\\d+:\\d+,\\d+"),
-      end =
-        sub[2] %>%
-        stringr::str_extract("\\d+:\\d+:\\d+,\\d+$"),
-      text =
-        sub[3:length(sub)] %>%
-        paste(collapse = "\n") %>%
-        stringr::str_replace("\n+$", "")
-    )
-
-  })
+  tibble::data_frame(
+    raw =
+      readr::read_file(file) %>%
+      stringr::str_split("(\r\n){2,}|\n{2,}") %>%
+      unlist()
+  ) %>%
+    separate(raw, c("index", "time", "text"), sep = "(\r\n)|\n", extra = "merge") %>%
+    separate(time, c("start", "end"), sep = " --> ")
 
 }
